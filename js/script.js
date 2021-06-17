46d80171a4a3f1f64176d4627fbf78d9bb5033fb
leonardo <sponzal4@gmail.com>
2021-06-17 15:04:35 +0200

    snack1

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// snack1
+
+const gatti = [
+    {nome:'alemanno', colore: 'nero', età: '2', sesso:'maschio'},
+    {nome:'lala', colore: 'bianco', età: '7', sesso:'femmina'},
+    {nome:'oslo', colore: 'nero', età: '4', sesso:'maschio'},
+    {nome:'eva', colore: 'marrone', età: '5', sesso:'femmina'}
+];
+gatti.forEach((element) => {
+console.log(`${element.nome} ${element.colore} ${element.età} ${element.sesso}`);
+});
+
+// snack2
